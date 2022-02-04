76fbfa650d280c7d611e19a714375b67a605509c
neumont-gamedev <rmaple@neumont.edu>
2022-02-04 15:22:39 -0700

    class 020422

---
+++ Assets/Scripts/StateAgent/IdleState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+	float timer;
+
+	public IdleState(StateAgent owner, string name) : base(owner, name)
+	{
+	}
+
+	public override void OnEnter()
+	{
+		Debug.Log(name + " enter");
+
+		timer = 2;
+	}
+
+	public override void OnExit()
+	{
+		Debug.Log(name + " exit");
+	}
+
+	public override void OnUpdate()
+	{
+		Debug.Log(name + " update");
+
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+		{
+			owner.stateMachine.SetState(owner.stateMachine.StateFromName("patrol"));
+		}
+	}
+}
--- Assets/Scripts/StateAgent/StateAgent.cs
+++ Assets/Scripts/StateAgent/StateAgent.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class StateAgent : Agent
 {
-    // Start is called before the first frame update
+    public AgentPath path;
+    public StateMachine stateMachine = new StateMachine();
+
     void Start()
     {
-        
+        stateMachine.AddState(new IdleState(this, "idle"));
+        stateMachine.AddState(new PatrolState(this, "patrol"));
+        stateMachine.SetState(stateMachine.StateFromName("idle"));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        stateMachine.Update();
+
+
+        if (movement.velocity.magnitude > 0.5f)
+		{
+            animator.SetBool("walk", true);
+		}
+        else
+		{
+            animator.SetBool("walk", false);
+        }
     }
+
+	private void OnGUI()
+	{
+        GUI.Label(new Rect(10, 10, 300, 20), stateMachine.GetStateName());
+	}
 }
