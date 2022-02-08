f0007df128f1fa3538598e6f6ac47a0cca398092
neumont-gamedev <rmaple@neumont.edu>
2022-02-07 18:28:54 -0700

    class 020722
    state transitions

--- Assets/Scripts/StateAgent/ChaseState.cs
+++ Assets/Scripts/StateAgent/ChaseState.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleState : State
+public class ChaseState : State
 {
-	public IdleState(StateAgent owner, string name) : base(owner, name)
+	public ChaseState(StateAgent owner, string name) : base(owner, name)
 	{
 	}
 
 	public override void OnEnter()
 	{
+		owner.movement.Resume();
 		Debug.Log(name + " enter");
-		owner.movement.Stop();
-		owner.timer.value = 2;
 	}
 
 	public override void OnExit()
@@ -22,6 +21,7 @@ public class IdleState : State
 
 	public override void OnUpdate()
 	{
+		owner.movement.MoveTowards(owner.enemy.transform.position);
 		Debug.Log(name + " update");
 	}
 }
