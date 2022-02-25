527fb10dab862a2554e9dc88bd5cd8568f6f9d81
neumont-gamedev <rmaple@neumont.edu>
2022-02-24 23:40:07 -0700

    class 022322

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -1,33 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UtilityAgent : Agent
 {
+    [SerializeField] Perception perception;
+    [SerializeField] MeterUI meter;
+
     Need[] needs;
+    UtilityObject activeUtilityObject = null;
+
+    public float happiness
+	{
+        get
+		{
+            float totalMotive = 0;
+			foreach (var need in needs)
+			{
+				totalMotive += need.motive;
+			}
+
+            return 1 - (totalMotive / needs.Length);
+		}
+	}
+
 
     void Start()
     {
         needs = GetComponentsInChildren<Need>();
+
+        meter.text.text = "";
     }
     
     void Update()
     {
-        
+        animator.SetFloat("speed", movement.velocity.magnitude);
+
+        if (activeUtilityObject == null)
+		{
+            var gameObjects = perception.GetGameObjects();
+            List<UtilityObject> utilityObjects = new List<UtilityObject>();
+            foreach (var go in gameObjects)
+			{
+                if (go.TryGetComponent<UtilityObject>(out UtilityObject utilityObject))
+				{
+                    utilityObject.visible = true;
+                    utilityObject.score = GetUtilityObjectScore(utilityObject);
+                    utilityObjects.Add(utilityObject);
+				}
+			}
+		}
     }
 
-    private void OnGUI()
-    {
-        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
-
-        GUI.color = Color.black;
-        int offset = 0;
-        foreach (var need in needs)
-        {
-            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.value);
-            offset += 20;
-        }
-        //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
+	private void LateUpdate()
+	{
+        meter.slider.value = happiness;
+        meter.worldPosition = transform.position + Vector3.up * 4;
     }
 
+    float GetUtilityObjectScore(UtilityObject utilityObject)
+	{
+        float score = 0;
+
+        foreach (var effector in utilityObject.effectors)
+		{
+            Need need = GetNeedByType(effector.type);
+            if (need != null)
+			{
+                float futureNeed = need.getMotive(need.input + effector.change);
+                score += need.motive - futureNeed;
+			}
+		}
+
+        return score;
+	}
+
+    Need GetNeedByType(Need.Type type)
+	{
+        return needs.First(need => need.type == type);
+	}
 }
